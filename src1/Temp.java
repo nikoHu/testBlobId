ee4c88d3eabd8963d267b4f55ebc8884e5dd2c04
nikoHu <43558903+nikoHu@users.noreply.github.com>
2021-06-09 15:53:09 +0800

    非功能变化代码示例
    非功能变化代码示例

---
+++ src1/Temp.java
@@ -0,0 +1,26 @@
+public class Temp {
+    public static void main(String[] args) {
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        if (log.isDebugEnabled())
+            log.debug(sm.getString("deltaManager.receiveMessage.delta", getName(), msg.getSessionID()));
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        Class<?>[] inf = type.getInterfaces();
+        Method mp = null;
+        for (Class<?> aClass : inf) {
+            ***
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        if (!deploymentDescriptor.exists()) {
+            throw new BuildException("Deployment descriptor: " + deploymentDescriptor + " does not exist.");
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        i = (b + a) / 2;
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        Element currentTest;
+        if (test != null) {
+            currentTest = testElements.get(createDescription(test));
+        } else {
+            currentTest = rootElement;
+        }
+    }
+}
---
+++ src1/Temp1.java
@@ -0,0 +1,22 @@
+public class Temp1 {
+    public static void main(String[] args) {
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        if (log.isDebugEnabled()) {
+            log.debug(sm.getString("deltaManager.receiveMessage.delta", getName(), msg.getSessionID()));
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        Class<?>[] interfaces = type.getInterfaces();
+        Method mp = null;
+        for (Class<?> iface : interfaces) {
+            ***
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        if (!deploymentDescriptor.exists()) {
+            throw new BuildException("Deployment descriptor: %s does not exist.", deploymentDescriptor);
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        i = (b + a) >>> 1;
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        Element currentTest = test == null ? rootElement : testElements.get(createDescription(test));
+    }
+}
